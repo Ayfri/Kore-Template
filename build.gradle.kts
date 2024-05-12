0873e8bc907d24f2c33d99b33fc3661769843fa0
Ayfri <pierre.ayfri@gmail.com>
2024-05-12 02:58:32 +0200

    chore(gradle): Set up Gradle build with Kotlin and Kore, activate parallel and caching configurations for optimized performance.

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+	// Apply the kotlin.jvm plugin to add support for Kotlin.
+	alias(libs.plugins.kotlin)
+
+	// Apply the application plugin to add support for running the application.
+	application
+}
+
+repositories {
+	// Use Maven Central for resolving dependencies.
+	mavenCentral()
+}
+
+dependencies {
+	// Install Kore.
+	implementation(libs.kore)
+}
+
+// Apply a specific Java toolchain to ease working on different environments.
+kotlin {
+	jvmToolchain(21)
+}
+
+application {
+	// Define the main class for the application.
+	mainClass = "MainKt"
+}
